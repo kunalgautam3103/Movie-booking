4e61b2e0339ffd75c151d2c758820bc8f3841138
kunalgautam3103 <kunalgautam3103@gmail.com>
2021-11-12 19:32:50 +0530

    checkpoint 3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Header from '../src/common/header/Header';
+import Home from './screens/home/Home';
 
 
-ReactDOM.render(<Header/>,document.getElementById('root'));
+ReactDOM.render(<Home></Home>,document.getElementById('root'));
 
